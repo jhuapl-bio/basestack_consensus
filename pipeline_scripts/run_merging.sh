2e1f95be925447c8d02345ffd0cbc888df4023b7
Melanie Kirsche <mjkirsche@gmail.com>
2020-04-24 12:42:15 -0400

    Added initial merging script

---
+++ pipeline_scripts/run_merging.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+if [ "$(uname -s)" = 'Linux' ]; then
+    BINDIR=$(dirname "$(readlink -f "$0" || echo "$(echo "$0" | sed -e 's,\\,/,g')")")
+else
+    BINDIR=$(dirname "$(readlink "$0" || echo "$(echo "$0" | sed -e 's,\\,/,g')")")
+fi
+
+# specify a sequencing run directory
+RUN=$1
+
+# get known directory names
+DIR="/home/idies/workspace/covid19/sequencing_runs/$RUN/artic-pipeline/3-hac-medaka-norm200"
+
+REF="$BINDIR/VariantValidator/nCoV-2019.reference.fasta"
+
+# make and save output directory
+outdir="/home/idies/workspace/Temporary/mkirsche/scratch/covid/$RUN/"
+if [ ! -d $outdir ]; then
+	mkdir $outdir
+fi
+
+for consfile in $DIR/*.consensus.fasta; do
+
+	sample=${consfile##*/}
+	samplename=${sample%%_*}
+
+	# loop through all NTC samples
+	if [ ! "$samplename" = "NTC" ]; then
+
+		echo $samplename
+
+		nanopolishvcffile="/home/idies/workspace/covid19/sequencing_runs/$RUN/artic-pipeline/4-draft-consensus-nanopolish/$samplename*.pass.vcf.gz"
+        nanopolishvcfunzipped=${nanopolishvcffile::-3}
+        if [ ! -r $nanopolishvcfunzipped ]
+        then
+            gunzip -c $nanopolishvcffile > $nanopolishvcfunzipped          
+        fi
+
+		medakavcffile="/home/idies/workspace/covid19/sequencing_runs/$RUN/artic-pipeline/4-draft-consensus_update/$samplename*.pass.vcf.gz"
+        medakavcfunzipped=${medakavcffile::-3}
+        if [ ! -r $medakavcfunzipped ]
+        then
+            gunzip -c $medakavcffile > $medakavcfunzipped          
+        fi
+
+		bamfile="$DIR/$samplename*.primertrimmed.rg.sorted.bam"
+		consensus="$DIR/$samplename*.consensus.fasta"
+		prefix=`echo $consensus`
+		prefix=${prefix##*/}
+		prefix=${prefix%%.*}
+
+        echo 'Running merging'
+        echo 'Reference: '$REF
+        echo 'Bam file: '$bamfile
+        echo 'Nanopolish VCF: '$nanopolishvcfunzipped
+        echo 'Medaka VCF: '$medakavcfunzipped
+        echo 'Out prefix: '$outdir/$prefix
+
+		# run script
+		$BINDIR/VariantValidator/run.sh $REF $bamfile $nanopolishvcfunzipped,$medakavcfunzipped $outdir/$prefix
+	fi
+done
+
