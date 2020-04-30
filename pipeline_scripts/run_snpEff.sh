5d656eec68e23d9ca32375b6c48a37d07635250a
shwohl <shirlee.wohl@gmail.com>
2020-04-30 20:19:08 +0000

    reverting to static form module 4 interactive notebook.. must copy this notebook to scratch for future usage

--- pipeline_scripts/run_snpEff.sh
+++ pipeline_scripts/run_snpEff.sh
@@ -10,10 +10,10 @@ fi
 RUN=$1
 
 if [ $RUN == "test_data" ]; then
-   DIR="/home/idies/workspace/covid19/sequencing_runs/test_data/artic-pipeline/5-post-filter_nanopolish/"
+   DIR="/home/idies/workspace/covid19/sequencing_runs/test_data/artic-pipeline/5-post-filter/"
    echo "Running SnpEff on test data "
 else
-   DIR="/home/idies/workspace/covid19/sequencing_runs/$RUN/artic-pipeline/5-post-filter_nanopolish"
+   DIR="/home/idies/workspace/covid19/sequencing_runs/$RUN/artic-pipeline/5-post-filter"
    
 OUTDIR=$DIR
 
@@ -26,7 +26,12 @@ DBNAME="ncov"
 #mkdir -p $OUTDIR
 
 #for vcf in $TEST_DIR/*.consensus.combined.vcf; do
-for vcf in $DIR/*.consensus.combined.vcf; do
+for vcf in $DIR/*.allsnps.combined.vcf; do
     ${SCRIPT_DIR}/annotate_variants.sh ${vcf} ${CONFIG} ${DBNAME} ${OUTDIR}
     echo "SnpEff completed on run ${DIR}"
 done
+
+echo "Making final reports on run ${DIR}"
+cat ${OUTDIR}/MDHP*_ann_report.txt  | awk '$4 != "N" { print $0}'  | awk '!seen[$0]++' > ${OUTDIR}/final_snpEff_report.txt
+cat ${OUTDIR}/MDHP*_ann_report.txt  | awk '!seen[$0]++' | awk 'NR == 1  || $4 == "N" { print $0}'  > ${OUTDIR}/snpEff_report_with_Ns.txt
+echo "DONE"
