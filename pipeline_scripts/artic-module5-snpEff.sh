e84c5011a74b3a7603e4a81088a845a90f8d9337
howsercw <craig.howser@jhuapl.edu>
2020-05-15 12:08:01 -0400

    automated script_dir path finding

--- pipeline_scripts/artic-module5-snpEff.sh
+++ pipeline_scripts/artic-module5-snpEff.sh
@@ -30,9 +30,15 @@ done
 
 DBNAME="ncov"
 
+annotate=$(which annotate_variants.sh)
+if [[ -z "$annotate" ]]; then
+	echo "Error: annotate_variants.sh is not in your path! Please place in your path before rerunning module."
+	exit 1
+fi
+
 while read barcode name; do
     vcf="${name}"_"${barcode}".allsnps.combined.vcf
-    "${SCRIPT_DIR}"/annotate_variants.sh "${vcf}" "${snpEff_config}" "${DBNAME}" "${postfilter_dir}"
+    bash -x "${annotate}" "${vcf}" "${snpEff_config}" "${DBNAME}" "${postfilter_dir}"
     echo "SnpEff completed on run ${postfilter_dir}"
     echo "Making final reports on run ${postfilter_dir}"
     cat "${postfilter_dir}"/"${name}"_"${barcode}"_ann_report.txt  | awk '$4 != "N" { print $0}'  | awk '!seen[$0]++' >> "${postfilter_dir}/final_snpEff_report.txt"
