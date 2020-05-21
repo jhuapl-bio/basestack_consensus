722204b67a661e364acbd979baea027938f66a1f
Tom Mehoke <tmehoke@gmail.com>
2020-05-20 20:46:45 -0400

    accounting for zero-indexed BED; unique start bug fix

--- pipeline_scripts/scheme_amplicon_profile.sh
+++ pipeline_scripts/scheme_amplicon_profile.sh
@@ -14,11 +14,11 @@ awk -F $'\t' 'BEGIN{
 	primer_stop[amplicon][a[3]] = (q_stop ? (q_stop > $3 ? q_stop : $3) : $3);
 } END {
 	for(amplicon in primer_start) {
-		start_f = primer_start[amplicon]["LEFT"];
+		start_f = primer_start[amplicon]["LEFT"]+1;
 		stop_f = primer_stop[amplicon]["LEFT"];
-		unique_start = (primer_start[amplicon-1]["RIGHT"] ? primer_start[amplicon-1]["RIGHT"]+1 : primer_stop[amplicon]["LEFT"]+1);
-		unique_stop = (primer_start[amplicon+1]["LEFT"] ? primer_start[amplicon+1]["LEFT"]-1 : primer_start[amplicon]["RIGHT"]-1);
-		start_r = primer_start[amplicon]["RIGHT"];
+		unique_start = (primer_start[amplicon-1]["RIGHT"] ? primer_stop[amplicon-1]["RIGHT"]+1 : primer_stop[amplicon]["LEFT"]+1);
+		unique_stop = (primer_start[amplicon+1]["LEFT"] ? primer_start[amplicon+1]["LEFT"] : primer_start[amplicon]["RIGHT"]+1);
+		start_r = primer_start[amplicon]["RIGHT"]+2;
 		stop_r = primer_stop[amplicon]["RIGHT"];
 		printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n", amplicon, start_f, stop_f, unique_start, unique_stop, start_r, stop_r);
 	}
