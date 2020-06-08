f05f9d1ad4aca27df2d8ba021c62272bb265a912
Tom Mehoke <tmehoke@gmail.com>
2020-06-08 14:30:25 -0400

    minor awk fix

--- pipeline_scripts/report_summary_table.sh
+++ pipeline_scripts/report_summary_table.sh
@@ -412,13 +412,13 @@ while read barcode label; do
 			for(amplicon in mask_start) {
 				start=mask_start[amplicon];
 				stop=mask_stop[amplicon];
-				min[amplicon]=999999999999;
+				depth_min[amplicon]=999999999999;
 				for(i=start; i<=stop; i++) {
 					if(depth[i] <= min) {
-						min[amplicon] = depth[i];
+						depth_min[amplicon] = depth[i];
 					}
 				}
-				if(min[amplicon] <= THRESH) {
+				if(depth_min[amplicon] <= THRESH) {
 					for(i=start; i<=stop; i++) {
 						printf("%s\n", i);
 					}
